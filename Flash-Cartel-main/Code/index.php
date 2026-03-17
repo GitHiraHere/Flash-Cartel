b35a3e51d635e769cce7b447e547d93f70994641
Rene Thomas <U0568426@unimail.hud.ac.uk>
2026-03-17 13:20:54 +0000

    Updated files to match Rene's repo

--- Flash-Cartel-main/Code/index.php
+++ Flash-Cartel-main/Code/index.php
@@ -1,25 +1,36 @@
 <?php
 $pageTitle = "Flash Cartel login page";
 include 'header.php';
+
+// Check if user is already signed in, and if so, send them to home page.
+if (array_key_exists("user_id", $_SESSION)) {
+  header("location: home.php");
+  exit();
+}
+
+// Error handling for returned login form.
+if (isset($_GET["error"])) {
+  if ($_GET["error"] === "emptyinput") {
+    echo "<p> Please fill in the form carefully! </p>";
+  } else if ($_GET["error"] === "wronglogin") {
+    echo "<p> Incorrect login information! </p>";
+  }
+}
 ?>
 
 <body>
-  <br />
-  <div class="centre">
-    <img
-      src="images/logo.jpg"
-      alt="Flash Cartel logo"
-      width="360"
-      height="180" />
-  </div>
-  <h2>LOGIN</h2>
-  <br>
   <form action="includes/login.inc.php" method="post">
+    <br />
+    <div class="centre">
+      <img src="images/logo.jpg" alt="Flash Cartel logo" class="logo" />
+    </div>
+    <h2>LOGIN</h2>
+    <br>
     <label for="username">Username:</label><br />
     <input type="text" id="username" name="username" placeholder="Username..." /><br />
     <label for="pwd">Password:</label><br />
     <input type="password" id="pwd" name="pwd" placeholder="Password..." />
-    <input type="submit" value="Submit" />
+    <input type="submit" name="submit" value="Submit" />
   </form>
   <br>
   <p>or click <a href="forgot.php">I forgot my password</a></p>
@@ -28,14 +39,4 @@ include 'header.php';
   <br>
 
   <?php
-  if (isset($_GET["error"])) {
-    if (isset($_GET["error"]) === "emptyinput") {
-      echo "<p> Please fill in the form carefully! </p>";
-    } else if (isset($_GET["error"]) === "wronglogin") {
-      echo "<p> Incorrect login information! </p>";
-    }
-  }
-  ?>
-</body>
-
-</html>
+  include "footer.php";
