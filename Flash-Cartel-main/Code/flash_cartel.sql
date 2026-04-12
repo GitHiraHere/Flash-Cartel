8a0bf79d73738d9519bf2a55419fb1566b75fd59
Rene Thomas <U0568426@unimail.hud.ac.uk>
2026-04-12 11:48:53 +0100

    Revert "Edited flash_cartel.sql to fix import error"
    This reverts commit 889c3610910d61fee73b499c5156cd8c92e04df8.

--- Flash-Cartel-main/Code/flash_cartel.sql
+++ Flash-Cartel-main/Code/flash_cartel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 25, 2026 at 05:30 PM
+-- Generation Time: Mar 02, 2026 at 01:18 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -27,55 +27,22 @@ SET time_zone = "+00:00";
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-  `user_id` int(11) PRIMARY KEY,
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(30) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Table structure for table 'decks'
---
-
-DROP TABLE IF EXISTS decks;
-
-CREATE TABLE decks (
-    deck_id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL REFERENCES users(user_id),
-    deck_name VARCHAR(30) NOT NULL
-    );
-
---
--- Table structure for table 'cards'
---
-
-DROP TABLE IF EXISTS cards;
-
-CREATE TABLE cards (
-    card_id INT PRIMARY KEY AUTO_INCREMENT,
-    deck_id INT NOT NULL REFERENCES decks(deck_id),
-    question VARCHAR(1000),
-    answer VARCHAR(1000)
-    );
-
 -- Indexes for dumped tables
 --
 
 --
 -- Indexes for table `users`
 --
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) AUTO_INCREMENT;
+  ADD PRIMARY KEY (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
